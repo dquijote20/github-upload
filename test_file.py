ab60627d62f23e8b767c130a978eac45c0b8bd8d
Don Quijote <dquijote20@gmail.com>
2020-05-09 00:08:16 -0400

    3rd commit

--- test_file.py
+++ test_file.py
@@ -7,7 +7,7 @@ Created on Fri May  8 23:10:42 2020
 
 import pandas as pd
 import numpy as np
-import datetime
+import datetime, time
 
 print("today's date:", datetime.date.today())
 
@@ -15,4 +15,4 @@ print("today's date:", datetime.datetime.now())
 
 for i in range(10):
     print("today's date:", datetime.datetime.now())
-    
+    time.sleep(0.5)
