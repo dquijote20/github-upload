7f13aa87865902e19d219d7b6f9de7a462b10238
Don Quijote <dquijote20@gmail.com>
2020-05-08 23:28:22 -0400

    Initial project commit

---
+++ test_file.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May  8 23:10:42 2020
+
+@author: NG18
+"""
+
+import pandas as pd
+import numpy as np
+import datetime
+
+print("today's date:", datetime.date.today())
